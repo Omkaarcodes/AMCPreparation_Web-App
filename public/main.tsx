89b351a809aa5cbe7b8e43e7896eebe046874c2d
Omkaar Sampigeadi <145941839+Omkaarcodes@users.noreply.github.com>
2025-07-13 00:25:48 -0400

    headstart on auth and login page design (should prob move to a differnet branch now).

--- public/main.tsx
+++ public/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AMCLandingPage from '../client/src/pages/Landing'
 import './globals.css'
-import TestComponent from '../TestComponent'
+import LoginForm from '../client/src/components/login-form'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TestComponent />
+    <LoginForm />
   </React.StrictMode>,
 )
